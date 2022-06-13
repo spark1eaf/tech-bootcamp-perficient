3e26e7bb010c21ac58c002171424684f11a45f8d
Scott Hebert <scott.phebert@yahoo.com>
2022-06-13 17:30:46 -0500

    Initial project setup and connection to db

---
+++ src/main/java/com/perficient/techbootcampScott/TechbootcampScottApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampScott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampScottApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampScottApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perficient/techbootcampScott/TechbootcampScottApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampScott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbootcampScottApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
